677b69f2ce2490250796fb806e14f0b5ccc300d3
Chandan-97 <cp564738@gmail.com>
2019-01-14 15:15:53 +0530

    Adding Tables

--- CTable/CTable/settings.py
+++ CTable/CTable/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+APPEND_SLASH = True
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
--- CTable/main/urls.py
+++ CTable/main/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 
 from . import views as mainviews
 
+
+
 urlpatterns = [
-    path('home/', mainviews.home),
-    path('search/', mainviews.search),
-    path('<int:uid>/', mainviews.getUserDetails),
-    path('', mainviews.redirectHome)
+    # path('home/', mainviews.home),
+    # path('search/', mainviews.search),
+    # path('<int:uid>/', mainviews.getUserDetails),
+    # path('', mainviews.redirectHome)
 ]
